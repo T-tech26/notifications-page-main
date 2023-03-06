3c859acc952482b443041b7db255b8d5fc984b8a
T-tech26 <94144505+T-tech26@users.noreply.github.com>
2023-03-06 08:40:56 +0530

    folder path update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /* @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./docs/**/*.{html,js}"],
   theme: {
     screens: {
       'sm' : '200px',
